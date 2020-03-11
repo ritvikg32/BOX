6cb9168627ae3559f0efebfefeb301e63a3ad3b9
Ritvik Ghattuwar <41013877+ritvikg32@users.noreply.github.com>
2020-03-12 00:02:00 +0530

    Game screen resolution has been reset Game icon is updated

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import pygame as pg
 import numpy
 import math
+from sprites import *
 from settings import *
 #Initializing the pygame library
 pg.init()
@@ -8,36 +9,52 @@ pg.init()
 
 pg.display.set_caption(gameName)
 
-gameDisplay = pg.display.set_mode(SCREEN_DIM)
+gameDisplay = pg.display.set_mode((WIDTH,HEIGHT))
 
 gameRunning = True
 intro = True
+
 class Game():
 
     def __init__(self):
         #initializing the game window
         pg.init()
-        self.screen = pg.display.set_mode(SCREEN_DIM)
+        self.icon = pg.image.load("Drawables\\main_logo.png")
+        pg.display.set_icon(self.icon)
+        self.screen = pg.display.set_mode((WIDTH,HEIGHT))
         pg.display.set_caption(gameName)
         #self.clock = pg.time.clock()
         self.running = True
     def new(self):
-        pass
+        # initialize all variables and do all the setup for a new game
+        #self.all_sprites = pg.sprite.Group()
+        #self.walls = pg.sprite.Group()
+        #self.mobs = pg.sprite.Group()
+        #self.bullets = pg.sprite.Group()
+        # for row, tiles in enumerate(self.map.data):
+        #     for col, tile in enumerate(tiles):
+        #         if tile == '1':
+        #             Wall(self, col, row)
+        #         if tile == 'M':
+        #             Mob(self, col, row)
+        #         if tile == 'P':
+        #             self.player = Player(self, col, row)
+        self.player = Player(self, 5, 5)
+        self.camera = Camera(self.map.width, self.map.height)
+
     def game_menu(self):
         while intro:
             for event in pg.event.get():
                 if event.type == pg.QUIT:
                     pg.quit()
                     quit()
-                if event.type == pg.MOUSEBUTTONDOWN:
-                    x, y = event.pos
-                    if img_new_game.get_rect().collidepoint(x, y):
-                        self.run()
+
+
+
             #Background image
             img_new_game = pg.image.load("Drawables\\test_new.png")
-            bg = pg.image.load("Drawables\\menu_background.png")
+            bg = pg.image.load("Drawables\\menu_background_1024x768.png")
             self.screen.blit(bg,[0,0])
-            self.screen.blit(img_new_game,[400,200])
 
             pg.display.flip()
 
@@ -65,6 +82,9 @@ class Game():
         self.screen.fill(WHITE)
         #self.all_sprites.draw(self.screen)
         pg.display.flip()
+
+
+
 g  = Game()
 g.game_menu()
 g.show_start_screen()
--- settings.py
+++ settings.py
@@ -1,8 +1,16 @@
+import pygame as pg
 gameName = 'BOX'
-SCREEN_DIM = (800,416)
+WIDTH = 1024
+HEIGHT = 768
 FPS = 40
+PLAYER_ACC = 0.5
+PLAYER_FRICTION = 0.2
 
+TILESIZE = 32
 
+#Box Image
+box_img = pg.image.load('Drawables\\Box.png')
 #define COLORS
 WHITE = (255,255,255)
 BLACK = (0,0,0)
+GREEN = (0,255,0)
